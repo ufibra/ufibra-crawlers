1cc2571c016814c391ceab90c2ff80d5b7cc0095
ezradiniz <linux.esdras@gmail.com>
2018-05-31 07:26:00 -0300

    Minor fixes

--- crawlers/crawlers/spiders/wheyprotein_4fitclub.py
+++ crawlers/crawlers/spiders/wheyprotein_4fitclub.py
@@ -7,10 +7,10 @@ from crawlers.utils import parse, product_type
 class Wheyprotein4fitclubSpider(scrapy.Spider):
     name = 'wheyprotein-4fitclub'
     store = '4fitclub'
-    start_urls = ['http://www.4fitclub.com.br/whey-protein-s1000//']
+    start_urls = ['https://www.4fitclub.com.br/massa-muscular/whey-protein-proteina-cat.html']
 
     def parse(self, response):
-        products = response.css('.products li')
+        products = response.css('.products-grid .item')
         for product in products:
             yield self.parse_product(product)
 
@@ -19,15 +19,15 @@ class Wheyprotein4fitclubSpider(scrapy.Spider):
             yield scrapy.Request(url=next_url, callback=self.parse)
 
     def parse_product(self, product):
-        name = product.css('.product-name ::text').extract_first()
-        price = parse.parse_price(product.css('.price .sale ::text').extract_first())
-        image = 'https://www.4fitclub.com.br' + product.css('.photo img::attr(data-src)').extract_first()
+        name = product.css('.product-name a ::text').extract_first()
+        price = parse.parse_price(product.css('.price ::text').extract_first())
+        image = product.css('.product-image img::attr(src)').extract_first()
         weight = parse.parse_weight(name)
         item = self.to_item(store=self.store, name=name, price=price, image=image, weight=weight, category=product_type.WHEY_PROTEIN)
         return item
 
     def next_page(self, response):
-        href = response.css(".page-number .page-next > a::attr('href')").extract_first()
+        href = response.css(".pages a.i-next::attr('href')").extract_first()
         url = response.urljoin(href)
         return url if href else None
 
